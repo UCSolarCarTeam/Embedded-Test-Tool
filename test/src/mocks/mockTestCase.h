bd7efe81e99a2d8b5685e56b70f50c11b54f5efe
Enoch Tsang <etsang1@hotmail.com>
2016-02-24 18:43:40 -0700

    created mock test case

---
+++ test/src/mocks/mockTestCase.h
@@ -0,0 +1,12 @@
+#include <string>
+
+#include "gmock/gmock.h"
+
+#include "TestCase.hpp"
+#include "TestResult.hpp"
+
+class MockGameState : public TestCase {
+public:
+	MOCK_METHOD0(run, TEST_RESULT());
+	MOCK_CONST_METHOD0(getTestName, std::string());  
+};
