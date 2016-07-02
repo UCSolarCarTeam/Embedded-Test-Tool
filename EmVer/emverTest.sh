99ca258edcc67f2170c1f2e2fcfde26ab90623ed
Enoch Tsang <etsang1@hotmail.com>
2016-07-02 00:24:31 -0600

    update travis and organize makefile

--- EmVer/emverTest.sh
+++ EmVer/emverTest.sh
@@ -2,6 +2,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+make clean -C ../Example
 make -C ../Example
 if [ $? -eq 0 ]; then
     printf "\n${GREEN}|*******| EXAMPLE SOURCE COMPILATION SUCCESSFUL |*******|${NC}\n\n"
